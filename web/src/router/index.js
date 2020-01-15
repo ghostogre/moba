71db3596c63c0aec1a7d6e80ebd642ca094e8860
ghostogre <126ying@sina.cn>
2020-01-15 22:30:38 +0800

    feature 首页节目搭建 && fix 公共样式的错误修改

---
+++ web/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../views/Main.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+      }
+    ]
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ web/src/views/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-3 flex-row ai-center">
+      <img src="../assets/logo.png" height="30" alt="">
+      <div class="px-2">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-grey">团队成就更多</div>
+      </div>
+    </div>
+    <div class="nav"></div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
