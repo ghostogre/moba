dd2358f86f79a04a12f2212e01528c0c3a893ab7
ghostogre <126ying@sina.cn>
2020-03-13 22:57:06 +0800

    开启history路由

---
+++ web/src/skeleton-entry.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Skeleton from './views/Skeleton/index.vue'
+export default new Vue({
+  components: {
+    Skeleton
+  },
+  template: '<skeleton/>'
+})
+
--- web/vue.config.js
+++ web/vue.config.js
@@ -9,11 +9,12 @@ module.exports = {
   configureWebpack: config => {
     // config就是webpack.config.js
     if (ENV === 'production') {
+      // 服务器配置gzip, 只是为了可以兼容这种格式, 可以读取这种格式, 压缩为gzip还是需要前端做的
       config.plugins.push(new compressionWebpackPlugin({
-        algorithm: 'gzip',
+        algorithm: 'gzip', // 压缩方式默认是gzip
         test: /\.(js|css|html)$/,
-        threshold: 10240,
-        minRatio: 0.8
+        threshold: 10240, // 只处理大于此子节数的才处理
+        minRatio: 0.8 // 压缩的比例
       }))
       // 不打包第三方包
       config.externals = {
