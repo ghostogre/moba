3005310d6229a50984dbdbe9c412ad744555ffc5
ghostogre <126ying@sina.cn>
2020-03-17 16:12:19 +0800

    删除多余的js文件，除了预渲染页面添加懒加载

--- web/src/skeleton-entry.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Skeleton from './views/Skeleton/index.vue'
-export default new Vue({
-  components: {
-    Skeleton
-  },
-  template: '<skeleton/>'
-})
-
---
+++ web/src/views/Activity/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <canvas id="activity"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Activity',
+  methods: {
+    // loadImg (arr) {
+    //   return new Promise(resolve => {
+    //     let count = 0
+    //   })
+    // }
+  }
+}
+</script>
