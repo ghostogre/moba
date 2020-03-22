df3e5a45ba67c92916d8585cd55b84e6f6736eaa
ghostogre <126ying@sina.cn>
2020-03-22 21:55:08 +0800

    fix history配置

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express')
+// 路由开启history模式
 const history = require('connect-history-api-fallback')
 
 const app = express()
-
-app.use(history())
 
 app.set('secret', 'i23u123') // 设置到app
 
 app.use(require('cors')())
 app.use(express.json())
 app.use('/uploads', express.static(__dirname + '/uploads')) // 静态文件托管
-app.use('/admin/', express.static(__dirname + '/admin')) // admin编译后静态文件托管
-app.use('/', express.static(__dirname + '/web')) // web编译后静态文件托管
+app.use('/admin', express.static(__dirname + '/admin')) // admin编译后静态文件托管
+app.use('/', history(), express.static(__dirname + '/web')) // web编译后静态文件托管
 
 require('./plugins/db')(app)
 require('./routers/admin')(app)
