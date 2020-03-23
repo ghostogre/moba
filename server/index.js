0c77db76ec32a53957d2f5cb105d8cfed7704932
ghostogre <yingjhpad@icloud.com.cn>
2020-03-23 21:50:17 +0800

    fix server

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ app.set('secret', 'i23u123') // 设置到app
 
 app.use(require('cors')())
 app.use(express.json())
+app.use(history())
 app.use('/uploads', express.static(__dirname + '/uploads')) // 静态文件托管
-app.use('/admin', history(), express.static(__dirname + '/admin')) // admin编译后静态文件托管
-app.use('/', history(), express.static(__dirname + '/web')) // web编译后静态文件托管
+app.use('/admin', express.static(__dirname + '/admin')) // admin编译后静态文件托管
+app.use('/', express.static(__dirname + '/web')) // web编译后静态文件托管
 
 require('./plugins/db')(app)
 require('./routers/admin')(app)
