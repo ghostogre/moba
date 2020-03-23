4a04bc108a4b950623d23e6db62841adc4bd9540
ghostogre <126ying@sina.cn>
2020-03-23 22:46:38 +0800

    test history

--- server/index.js
+++ server/index.js
@@ -9,8 +9,15 @@ app.set('secret', 'i23u123') // 设置到app
 app.use(require('cors')())
 app.use(express.json())
 app.use('/uploads', express.static(__dirname + '/uploads')) // 静态文件托管
-app.use('/admin', history(), express.static(__dirname + '/admin')) // admin编译后静态文件托管
-app.use('/', history(), express.static(__dirname + '/web')) // web编译后静态文件托管
+// app.use('/admin', history(), express.static(__dirname + '/admin')) // admin编译后静态文件托管
+// app.use('/', history(), express.static(__dirname + '/web')) // web编译后静态文件托管
+app.use('/', async (req, res, next) => {
+  if (req.url.startsWith('/admin')) {
+    res.sendFile('./admin/index.html')
+  } else {
+    res.sendFile('./web/index.html')
+  }
+})
 
 require('./plugins/db')(app)
 require('./routers/admin')(app)
