58ceae25f917b307e6efeaef7c62e3d1af154701
ghostogre <yingjhpad@icloud.com.cn>
2020-03-24 22:19:28 +0800

    fix 关闭admin的hash模式

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // 路由开启history模式
-const history = require('connect-history-api-fallback')
+// const history = require('connect-history-api-fallback')
 
 const app = express()
 
@@ -8,16 +8,16 @@ app.set('secret', 'i23u123') // 设置到app
 
 app.use(require('cors')())
 app.use(express.json())
-app.use(history({
-  verbose: true
-}))
-app.use('/uploads', express.static(__dirname + '/uploads')) // 静态文件托管
-app.use('/admin', express.static(__dirname + '/admin')) // admin编译后静态文件托管
-app.use('/', express.static(__dirname + '/web')) // web编译后静态文件托管
 
 require('./plugins/db')(app)
 require('./routers/admin')(app)
 require('./routers/web')(app)
+app.use('/uploads', express.static(__dirname + '/uploads')) // 静态文件托管
+app.use('/admin', express.static(__dirname + '/admin')) // admin编译后静态文件托管
+app.use('/', history({
+  disableDotRule: true,
+  htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
+}), express.static(__dirname + '/web')) // web编译后静态文件托管
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
